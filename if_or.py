aee1c65b81d7b0ca30b0fb38a27bf900550f745c
unknown <jay.panicker91@gmail.com>
2024-08-16 09:35:56 +0100

    add files

---
+++ if_or.py
@@ -0,0 +1,11 @@
+has_good_credit = True
+has_criminal_record = True
+
+# if has_criminal_record or has_criminal_record:
+#     print("Eligible for Loan ")
+
+# if has_good_credit and has_good_credit:
+#     print("Eligible for Loan ")
+
+if has_good_credit and not has_good_credit:
+ print("Eligible for Loan ")
